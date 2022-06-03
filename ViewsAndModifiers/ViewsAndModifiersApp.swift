4d96e7d4af0c3038dec03ddbf7cab9e8c2a53214
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-03 10:22:37 -0400

    Initial Commit

---
+++ ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Gabriel Marquez on 2022-06-03.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
