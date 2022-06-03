b0204bb5c91622cfdb207d1095c1d53313f3ca7e
Gabriel Marquez <gabrielmarquezrivas@gmail.com>
2022-06-03 14:18:35 -0400

    SwiftUI view modifier

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -10,12 +10,23 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.indigo)
             .padding()
+            .background(.red)
+            .padding()
+            .background(.blue)
+            .padding()
+            .background(.green)
+            .padding()
+            .background(.yellow)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+            .previewInterfaceOrientation(.portrait)
     }
 }
